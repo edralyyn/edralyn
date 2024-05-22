1ba967d40419cf34966814df2b44ad06cfd28423
edralyyn <edralyyyyynperaltaa@gmail.com>
2024-05-22 17:37:59 +0800

    hbkjn

--- components/SubmitReport.js
+++ components/SubmitReport.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Animated } from 'react-native';
-import SubmittedModal from './SubmittedModal';
+import { Modal, Text, TouchableOpacity, View, TouchableWithoutFeedback, Animated } from 'react-native';
 import screenStyles from './styles/screenStyles';
 
 const SubmitReport = () => {
@@ -21,7 +20,24 @@ const SubmitReport = () => {
           <Text style={screenStyles.buttonText}>Submit</Text>
         </TouchableOpacity>
       </Animated.View>
-      <SubmittedModal visible={modalVisible} closeModal={closeModal} />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={closeModal}
+      >
+        <View style={screenStyles.modalContainer}>
+          <TouchableWithoutFeedback onPress={closeModal}>
+            <View style={screenStyles.modalOverlay} />
+          </TouchableWithoutFeedback>
+          <View style={screenStyles.modalContent}>
+            <Text style={screenStyles.modalText}>Report submitted!</Text>
+            <TouchableOpacity style={screenStyles.button} onPress={closeModal}>
+              <Text style={screenStyles.buttonText}>OK</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
