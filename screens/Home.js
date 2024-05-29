4a1716d4fc3c9f81e6cb087e9c217220bf9aa8e6
edralyyn <edralyyyyynperaltaa@gmail.com>
2024-05-29 14:01:12 +0800

    homee

--- screens/Home.js
+++ screens/Home.js
@@ -1,40 +1,47 @@
-// screens/Home.js
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import ReportIcons from '../components/ReportIcons';
+import screenStyles from '../components/styles/screenStyles';
 
 const HomeScreen = ({ navigateToScreen }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      {/* You can add your home screen components here */}
-      <TouchableOpacity style={styles.button} onPress={() => navigateToScreen('Profile')}>
-        <Text style={styles.buttonText}>Go to Profile</Text>
+    <View style={screenStyles.container}>
+      <View style={styles.headerContainer}>
+        <View style={styles.textContainer}>
+          <Text style={[styles.text, { color: '#2A2D90', fontSize: 25, marginBottom: -5 }]}>Project</Text>
+          <Text style={[styles.text, { color: '#7B0000', marginTop: -5 }]}>
+            RE<Text style={{ color: '#FFB700', fontSize: 28 }}>SOLVE</Text>
+          </Text>
+        </View>
+        <Image source={require('../assets/logos.png')} style={styles.image} />
+      </View>
+      <TouchableOpacity style={screenStyles.button} onPress={() => navigateToScreen('Profile')}>
+        <Text style={screenStyles.buttonText}>Go to Profile</Text>
       </TouchableOpacity>
-      <ReportIcons/>
+      <ReportIcons />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
+  headerContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
+    justifyContent: 'space-between',
     marginBottom: 20,
   },
-  button: {
-    padding: 15,
-    backgroundColor: '#007BFF',
-    borderRadius: 10,
+  textContainer: {
+    flex: 1, // Take up remaining space
+    alignItems: 'flex-start', // Align the text to the end (right)
   },
-  buttonText: {
-    color: '#FFF',
-    fontSize: 16,
+  image: {
+    width: 200, // Adjust width as needed
+    height: 100, // Adjust height as needed
   },
+  text: {
+    fontSize: 35,
+    fontWeight: '800',
+  }
 });
 
 export default HomeScreen;
