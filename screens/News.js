096aa49cad42b859e7c92ed4ada2e0595fb6a43f
edralyyn <edralyyyyynperaltaa@gmail.com>
2024-05-31 17:16:09 +0800

    dasda

--- screens/News.js
+++ screens/News.js
@@ -1,32 +1,44 @@
 // News.js
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Background from '../components/Bred';
+import { Text, View, StyleSheet, ScrollView, Image } from 'react-native';
 
 const News = () => {
     return (
-        <Background>
+
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
             <View style={styles.container}>
+                <Image source={require('../assets/cover1.png')}/>
                 <Text style={styles.text}>
                     Here is some news content.
                 </Text>
             </View>
-        </Background>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
+    scrollContainer: {
+        flex: 1,
+    },
+    image: {
+        padding: -20,
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        borderColor: 'black',
+        borderWidth: 2,
+        width: '100%',
     },
     text: {
         fontSize: 18,
         fontWeight: 'bold',
         textAlign: 'center',
-        color: 'white',
+        color: 'black',
+        marginTop: 10,
     },
 });
 
 export default News;
+
